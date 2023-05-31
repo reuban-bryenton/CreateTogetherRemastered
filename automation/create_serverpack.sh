6b4b71279e83b01ed5c20fa69d2fdfac22d07207
Reuban Bryenton <r.bryenton2003@gmail.com>
2023-06-01 00:59:51 +0200

    Initial Commit [v1.4-pre1]

---
+++ automation/create_serverpack.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+echo "This script is used to create the server pack for CurseForge\n"
+echo "Error: this scipt is not available yet\n"
---
+++ automation/launch_server.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+echo "This script is used to create and launch the server\n"
+echo "Error: this scipt is not available yet\n"
